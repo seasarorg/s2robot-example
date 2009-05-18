28f43b0402f852afc32f2535a9da23dbe8c12698
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2009-05-18 20:52:11 +0000

    initial commit.
    git-svn-id: https://www.seasar.org/svn/sandbox/s2robot/trunk@28 a24fa471-b002-4554-a963-9a0eab8552e2

---
+++ s2robot-seo/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd `dirname $`
+BASE_DIR=`pwd`
+LIB_DIR=$BASE_DIR/lib
+S2ROBOT_EXAMPLE_JAR_FILE=`ls $BASE_DIR/target/s2robot-example-*.jar`
+CP_PATH=".:$S2ROBOT_EXAMPLE_JAR_FILE"
+
+for file in `ls $LIB_DIR/*.jar` ; do
+    CP_PATH="$CP_PATH:$file"
+done
+
+java -cp $CP_PATH org.seasar.robot.example.Crawler $@
+
